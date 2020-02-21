6526b65e184ba81cb6243eb80559af784e6234fa
aravinthsolai <52578860+aravinthsolai@users.noreply.github.com>
2020-02-21 14:37:25 +0530

    Update Trapping-Rain-Water.py

--- Trapping-Rain-Water.py
+++ Trapping-Rain-Water.py
@@ -1,4 +1,4 @@
-id = [1,0,2,1,0,1,3,2,1,2,1]
+id = [1,0,2,1,0,1,3,2,1,2,1,3]
 i=0
 j=1
 water_level=0
@@ -9,7 +9,8 @@ for x in id:
  #print(q)
  #print('first loop ' + str(x))
  t=[x]
- #print('initial array' + str(t))
+ #print('initial array ' + str(t))
+ #print('j value '+str(j))
  if i != q and i >= j:
   for y in id[i:q]:
    #print(id[i:])
@@ -18,9 +19,13 @@ for x in id:
     t.append(y)
     j=j+1
     #print('child 1 loop append' + str(t))
-   if y >= x:
+    if x == y:
+     break
+    
+   if y > x:
     #print('child 2 loop ' + str(y))
     t.append(y)
+    #print('child 2 loop append' + str(t))
     j=j+1
     if len(t) < 3:
      t=[x]
@@ -30,19 +35,21 @@ for x in id:
   t=[x]
  
  if len(t) >= 3:
-  #print(t)
-  while t[-1] <= t[-2] and len(t) > 2:
-   #print(t)
+  while t[-1] <= t[-2]:
    t.pop(-1)
+   #print('popped from here')
   while t[0] > t[-1]:
    t.pop(0)
+   #print('popped from here 2')
+  print('final array value '+ str(t))
   t.pop(-1)
   k=t
   k.sort()
   
   for v in t:
    water_level=water_level+(k[-1]-v)
-   
-print(water_level)
+  
+  
+print('water_level is '+ str(water_level))
   
   
